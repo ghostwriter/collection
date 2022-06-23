b2265af8b4607d3bbcf1a391636312d1a07d24f0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-22 20:46:15 -0500

    Update Collection.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Collection.php
+++ src/Collection.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Collection;
 
-use Ghostwriter\Collection\Contract\CollectionInterface;
-
 /**
  * @template TValue
  * @extends AbstractCollection<TValue>
- * @implements CollectionInterface<TValue>
  */
-final class Collection extends AbstractCollection implements CollectionInterface
+final class Collection extends AbstractCollection
 {
 }
