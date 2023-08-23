f27ebfe3eef722497c0c533d342e7a9f4f47cb41
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-23 05:39:19 -0500

    Update Collection.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Collection.php
+++ src/Collection.php
@@ -5,162 +5,176 @@ declare(strict_types=1);
 namespace Ghostwriter\Collection;
 
 use Closure;
-use Countable;
-use Generator;
-use Ghostwriter\Collection\Exception\CollectionException;
-use Ghostwriter\Option\Some;
-use Ghostwriter\Option\SomeInterface;
+use Ghostwriter\Collection\Exception\FirstValueNotFoundException;
+use Ghostwriter\Collection\Exception\LengthMustBePositiveIntegerException;
+use Ghostwriter\Collection\Exception\OffsetMustBePositiveIntegerException;
 use IteratorAggregate;
-use Traversable;
-
-use const PHP_INT_MAX;
-use function iterator_count;
-use function iterator_to_array;
 
 /**
  * @template TValue
  *
- * @implements IteratorAggregate<int,TValue>
+ * @implements IteratorAggregate<TValue>
  *
  * @see \Ghostwriter\Collection\Tests\Unit\CollectionTest
  */
-final class Collection implements Countable, IteratorAggregate
+final class Collection implements \Countable, \IteratorAggregate
 {
-    /** @param SomeInterface<Closure():Traversable<int,TValue>> $some */
+    /**
+     * @param \SplFixedArray<TValue> $storage
+     */
     private function __construct(
-        private SomeInterface $some
+        private readonly \SplFixedArray $storage
     ) {
     }
 
     /**
      * @param iterable<TValue> $iterable
+     *
+     * @return self<TValue>
      */
     public function append(iterable $iterable = []): self
     {
-        if ($iterable === []) {
+        if ([] === $iterable) {
             return $this;
         }
 
-        return self::fromGenerator(function () use ($iterable): Generator {
-            yield from $this;
-            yield from $iterable;
+        return self::fromGenerator(function () use ($iterable): \Generator {
+            foreach ($this->storage as $value) {
+                yield $value;
+            }
+
+            foreach ($iterable as $value) {
+                yield $value;
+            }
         });
     }
 
     /**
      * @template TContains
      *
-     * @param Closure(TValue,int):bool|TContains $functionOrValue
+     * @param (\Closure(TValue):bool)|TContains $functionOrValue
      */
     public function contains(mixed $functionOrValue): bool
     {
-        $function = $functionOrValue instanceof Closure ?
-            $functionOrValue :
-            static fn (mixed $value, mixed $_): bool => $value === $functionOrValue;
-
-        foreach ($this->getIterator() as $key => $value) {
-            if ($function($value, $key)) {
-                return true;
-            }
+        $function = $functionOrValue;
+        if (!$function instanceof \Closure) {
+            $function = static fn (mixed $value): bool => $value === $functionOrValue;
         }
 
-        return false;
+        /* @var Closure(TValue):bool $function */
+        return (bool) $this->filter($function)->count();
     }
 
     public function count(): int
     {
-        return iterator_count($this);
+        return $this->storage->count();
     }
 
     /**
      * @param int<0,max> $length
+     *
+     * @return self<TValue>
+     *
+     * @throws LengthMustBePositiveIntegerException
      */
     public function drop(int $length): self
     {
+        /* @psalm-suppress MissingThrowsDocblock OffsetMustBePositiveIntegerException */
         return $this->slice($length);
     }
 
     /**
-     * @param Closure(TValue,int):void $function
+     * @param \Closure(TValue):void $function
      */
-    public function each(Closure $function): void
+    public function each(\Closure $function): void
     {
-        foreach ($this->getIterator() as $key => $value) {
-            $function($value, $key);
+        foreach ($this->storage as $value) {
+            $function($value);
         }
     }
 
     /**
-     * @param Closure(TValue,int):bool $function
+     * @param \Closure(TValue):bool $function
+     *
+     * @return self<TValue>
      */
-    public function filter(Closure $function): self
+    public function filter(\Closure $function): self
     {
-        return self::fromGenerator(function () use ($function): Generator {
-            foreach ($this->getIterator() as $key => $value) {
-                if ($function($value, $key)) {
-                    yield $value;
+        return self::fromGenerator(function () use ($function): \Generator {
+            foreach ($this->storage as $value) {
+                if (!$function($value)) {
+                    continue;
                 }
+
+                yield $value;
             }
         });
     }
 
     /**
-     * @param ?Closure(TValue,int):bool $function
+     * @param ?\Closure(TValue):bool $function
      *
      * @return ?TValue
+     *
+     * @throws FirstValueNotFoundException If no value is found
      */
-    public function first(Closure $function = null): mixed
+    public function first(\Closure $function = null): mixed
     {
-        $function ??= static fn (mixed $value, int $_): bool => $value !== null;
-        foreach ($this->getIterator() as $key => $value) {
-            if ($function($value, $key)) {
-                return $value;
-            }
+        $function ??= static fn (mixed $value): bool => null !== $value;
+
+        foreach ($this->filter($function) as $value) {
+            return $value;
         }
 
-        return null;
+        throw new FirstValueNotFoundException();
     }
 
     /**
-     * @template TGenerator
+     * @param \Closure():\Generator $generator
      *
-     * @param Closure():Traversable<int,TGenerator> $generator
+     * @return self<TValue>
      */
-    public static function fromGenerator(Closure $generator): self
+    public static function fromGenerator(\Closure $generator): self
     {
-        $some = Some::create($generator);
+        /** @var \Closure():\Generator<TValue> $generator */
+        $collection = $generator();
 
-        return new self($some);
+        /** @var array<int,TValue> $asArray */
+        $asArray = \iterator_to_array($collection);
+
+        return new self(\SplFixedArray::fromArray($asArray, false));
     }
 
+    /**
+     * @return self<TValue>
+     */
     public static function fromIterable(iterable $iterable = []): self
     {
-        /**
-         * @var iterable<TValue> $iterable
-         */
+        /* @var iterable<TValue> $iterable */
         return self::fromGenerator(static fn () => yield from $iterable);
     }
 
-    public function getIterator(): Traversable
+    /**
+     * @return \Generator<TValue>
+     */
+    public function getIterator(): \Generator
     {
-        $closure = $this->some->unwrap();
-
-        yield from $closure();
+        yield from $this->storage;
     }
 
     /**
-     * @param ?Closure(TValue,int):bool $function
+     * @param ?\Closure(TValue):bool $function
      *
-     * @return null|TValue
+     * @return TValue|null
      */
-    public function last(?Closure $function = null): mixed
+    public function last(\Closure $function = null): mixed
     {
         $last = null;
-        $function ??= static fn (mixed $value, int $_): bool => $value !== null;
-        foreach ($this->getIterator() as $key => $value) {
-            if ($function($value, $key)) {
-                $last = $value;
-            }
+
+        $function ??= static fn (mixed $value): bool => null !== $value;
+
+        foreach ($this->filter($function) as $value) {
+            $last = $value;
         }
 
         return $last;
@@ -169,13 +183,15 @@ final class Collection implements Countable, IteratorAggregate
     /**
      * @template TMap
      *
-     * @param Closure(TValue,int):TMap $function
+     * @param \Closure(TValue):TMap $function
+     *
+     * @return self<TMap>
      */
-    public function map(Closure $function): self
+    public function map(\Closure $function): self
     {
-        return self::fromGenerator(function () use ($function): Generator {
-            foreach ($this->getIterator() as $key => $value) {
-                yield $function($value, $key);
+        return self::fromGenerator(function () use ($function): \Generator {
+            foreach ($this->storage as $value) {
+                yield $function($value);
             }
         });
     }
@@ -183,15 +199,15 @@ final class Collection implements Countable, IteratorAggregate
     /**
      * @template TAccumulator
      *
-     * @param ?TAccumulator                                      $accumulator
-     * @param Closure(null|TAccumulator,TValue,int):TAccumulator $function
+     * @param ?TAccumulator                                   $accumulator
+     * @param \Closure(TAccumulator|null,TValue):TAccumulator $function
      *
      * @return ?TAccumulator
      */
-    public function reduce(Closure $function, mixed $accumulator = null): mixed
+    public function reduce(\Closure $function, mixed $accumulator = null): mixed
     {
-        foreach ($this->getIterator() as $key => $value) {
-            $accumulator = $function($accumulator, $value, $key);
+        foreach ($this->storage as $value) {
+            $accumulator = $function($accumulator, $value);
         }
 
         return $accumulator;
@@ -201,30 +217,31 @@ final class Collection implements Countable, IteratorAggregate
      * @param int<0,max> $offset
      * @param int<0,max> $length
      *
-     * @throws CollectionException If $offset or $length are negative
-     *
-     * @noinspection PhpConditionAlreadyCheckedInspection
-     *
      * @psalm-suppress DocblockTypeContradiction
+     *
+     * @return self<TValue>
+     *
+     * @throws OffsetMustBePositiveIntegerException
+     * @throws LengthMustBePositiveIntegerException
      */
-    public function slice(int $offset, int $length = PHP_INT_MAX): self
+    public function slice(int $offset, int $length = \PHP_INT_MAX): self
     {
         if ($offset < 0) {
-            throw new CollectionException('$offset must be positive');
+            throw new OffsetMustBePositiveIntegerException();
         }
 
         if ($length < 0) {
-            throw new CollectionException('$length must be positive');
+            throw new LengthMustBePositiveIntegerException();
         }
 
         return self::fromGenerator(
-            function () use ($offset, $length): Generator {
+            function () use ($offset, $length): \Generator {
                 $total = 0;
 
                 if ($total !== $length) {
                     $limit = $offset + $length;
 
-                    foreach ($this->getIterator() as $current) {
+                    foreach ($this as $current) {
                         if ($total++ < $offset) {
                             continue;
                         }
@@ -242,17 +259,22 @@ final class Collection implements Countable, IteratorAggregate
 
     /**
      * @param int<0,max> $length
+     *
+     * @return self<TValue>
+     *
+     * @throws LengthMustBePositiveIntegerException
      */
     public function take(int $length): self
     {
+        /* @psalm-suppress MissingThrowsDocblock OffsetMustBePositiveIntegerException */
         return $this->slice(0, $length);
     }
 
     /**
-     * @return array<int,TValue>
+     * @return array<TValue>
      */
     public function toArray(): array
     {
-        return iterator_to_array($this, false);
+        return $this->storage->toArray();
     }
 }
