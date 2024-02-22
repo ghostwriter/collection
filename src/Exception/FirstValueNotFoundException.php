797dc4dc5bbfd14ed253a36af1a9dbf769840c9c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-22 13:48:41 -0600

    Update FirstValueNotFoundException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FirstValueNotFoundException.php
+++ src/Exception/FirstValueNotFoundException.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Collection\Exception;
 
-use Ghostwriter\Collection\ExceptionInterface;
+use Ghostwriter\Collection\Interface\ExceptionInterface;
+use RuntimeException;
 
-final class FirstValueNotFoundException extends \RuntimeException implements ExceptionInterface
+final class FirstValueNotFoundException extends RuntimeException implements ExceptionInterface
 {
 }
