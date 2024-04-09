7923bb6f87d9d4e8d274f74c7372039059fafe50
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 09:25:05 -0500

    Update OffsetMustBePositiveIntegerException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/OffsetMustBePositiveIntegerException.php
+++ src/Exception/OffsetMustBePositiveIntegerException.php
@@ -7,6 +7,4 @@ namespace Ghostwriter\Collection\Exception;
 use Ghostwriter\Collection\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
-final class OffsetMustBePositiveIntegerException extends InvalidArgumentException implements ExceptionInterface
-{
-}
+final class OffsetMustBePositiveIntegerException extends InvalidArgumentException implements ExceptionInterface {}
